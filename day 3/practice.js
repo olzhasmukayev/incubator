fae50b29112c50e248e6a5647942ec760d41a997
qwezeey <olzhas.mukayev@nu.edu.kz>
2022-06-08 21:07:29 +0600

    day 3

--- day 3/practice.js
+++ day 3/practice.js
@@ -47,3 +47,58 @@ if (workDone) {
 } else {
     console.log(`you should do your work`);
 }
+
+// using arrays for the practice
+// the dish-washer (by hand)
+
+let washer = []; // initially the dish washing place is empty 
+washer.push("dish");
+washer.unshift("dish"); 
+washer.unshift("plate");
+washer.unshift("cup");
+washer.unshift("fork");
+
+console.log(washer);
+console.log("---------washing the dishes----------\n");
+
+washer.shift();
+console.log(washer);
+washer.shift();
+console.log(washer);
+washer.shift();
+console.log(washer);
+washer.shift();
+console.log(washer);
+washer.shift();
+console.log(washer);
+console.log("---------dishes have been washed----------\n");
+
+let dishPlace = ["fork", "spoon", "plate"];
+let extraDishPlace = ["plate", "dish", "spoon"];
+
+let newDishPlace = dishPlace.concat(extraDishPlace);
+console.log(newDishPlace);
+
+let asked = "fork";
+
+console.log(`Olzhas please give me ${asked}\n`);
+if (newDishPlace.includes(asked)) {
+    console.log(`Hear is the ${asked}`);
+} else {
+    console.log(`we dont have ${asked}`);
+}
+
+let newPrintDish = newDishPlace.join("|");
+console.log(newPrintDish);
+
+console.log("we want to copy some of the dishes to the new dish place\n");
+
+let dishingPlace = newDishPlace.slice(0, 3);
+console.log(dishPlace);
+
+console.log("insert dish to the list\n");
+dishingPlace.splice(3, 0, "dish");
+console.log(dishingPlace);
+
+const arr = ["this is the correct array using const"];
+console.log(arr);
