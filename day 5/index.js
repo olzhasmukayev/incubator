6680a4b91be5e0be2be931d9046d842c207e8db8
olzhasmukayev <olzhasmukaev@gmail.com>
2022-06-10 10:43:14 +0600

    day 5

---
+++ day 5/index.js
@@ -0,0 +1,9 @@
+import chalk from "chalk";
+
+const log = console.error;
+let colors = ["yellow", "red", "blue"];
+
+for (let i of colors) {
+    console.log(i);
+}
+console.error("Some green text!");
